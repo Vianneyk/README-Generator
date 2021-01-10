dcae5c5db06fa296fee5eea6e4eb0d7506a25188
Vianney Kabamba <vianneyk@hotmail.co.uk>
2021-01-10 15:06:07 +0000

    questions added in index.js file

--- index.js
+++ index.js
@@ -1,5 +1,65 @@
 // array of questions for user
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./generateMarkdown");
+
 const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "Title"
+    },
+    {
+        type: "input",
+        message: "Please write a description of your project",
+        name: "Description"
+    },
+    {
+        type: "input", 
+        message: "Installation instruction",
+        name: "Install"
+    },
+    {
+        type: "input",
+        message: "Explain how to use the app",
+        name: "Usage"
+    },
+    {
+        type: "list",
+        message: "Which license are you using?",
+        name: "License",
+        choices: [
+            "MIT license",
+            "GVL GPL license",
+            "Apache license",
+            "No license"
+        ]
+    },
+    {
+        type: "input",
+        message: "Are there ny contributors?",
+        name: "Contributors"
+    },
+    {
+        type: "input", 
+        message: "How to test the app",
+        name: "Test"
+    },
+    {
+        type: "input", 
+        message: "Any questions?",
+        name: "Questions"
+    },
+    {
+        type: "input", 
+        message: "What is your Github Username?",
+        name: "Github"
+    },
+    {
+        type: "input", 
+        message: "What is your email?",
+        name: "Email"
+    }
 
 ];
 
