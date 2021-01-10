1756690270c2c0aa3eab6c4913ca7465c28f684a
Vianney Kabamba <vianneyk@hotmail.co.uk>
2021-01-10 20:04:46 +0000

    modified readme and index

--- README.md
+++ README.md
@@ -1,39 +1,37 @@
+# README Generator
 
+## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributors](#contributors)
+  * [Test](#test)
+  * [Questions](#questions)
+
+  ## Description
+  Use node.js to create a readme.md
+
+  ## Installation
+  Clone from my git hub repo
+
+  ## Usage
+  run node index.js in the terminal
+
+  ## License
+  This application is covered by the undefined license.
+
+  ## Contributors
+  None
+
+  ## Test
+  Run node index.js in the terminal
+
+  ## Questions
+  None
     
-    # README Generator
-
-    ## Table of Contents
-    * [Description](#description)
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [License](#license)
-    * [Contributors](#contributors)
-    * [Test](#test)
-    * [Questions](#questions)
-
-    ## Description
-    Use node.js to create a readme.md
-
-    ## Installation
-    Clone from my git hub repo
-
-    ## Usage
-    run node index.js in the terminal
-
-    ## License
-    This application is covered by the undefined license.
-
-    ## Contributors
-    None
-
-    ## Test
-    Run node index.js in the terminal
-
-    ## Questions
-    None
+  Find me on GitHub: [Vianneyk](https://github.com/Vianneyk)
     
-    Find me on GitHub: [Vianneyk](https://github.com/Vianneyk)
-    
-    Email me with any questions: vianneyk@hotmail.co.uk
+  Email me with any questions: vianneyk@hotmail.co.uk
 
-  # README-Generator
+  
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,42 +1,40 @@
 // function to generate markdown for README
 function generateMarkdown(response) {
-    return `
+    return `# ${response.Title}
+
+## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributors](#contributors)
+  * [Test](#test)
+  * [Questions](#questions)
+
+  ## Description
+  ${response.Description}
+
+  ## Installation
+  ${response.Install}
+
+  ## Usage
+  ${response.Usage}
+
+  ## License
+  This application is covered by the ${response.license} license.
+
+  ## Contributors
+  ${response.Contributors}
+
+  ## Test
+  ${response.Test}
+
+  ## Questions
+  ${response.Questions}
     
-    # ${response.Title}
-
-    ## Table of Contents
-    * [Description](#description)
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [License](#license)
-    * [Contributors](#contributors)
-    * [Test](#test)
-    * [Questions](#questions)
-
-    ## Description
-    ${response.Description}
-
-    ## Installation
-    ${response.Install}
-
-    ## Usage
-    ${response.Usage}
-
-    ## License
-    This application is covered by the ${response.license} license.
-
-    ## Contributors
-    ${response.Contributors}
-
-    ## Test
-    ${response.Test}
-
-    ## Questions
-    ${response.Questions}
+  Find me on GitHub: [${response.Github}](https://github.com/${response.Github})
     
-    Find me on GitHub: [${response.Github}](https://github.com/${response.Github})
-    
-    Email me with any questions: ${response.Email}
+  Email me with any questions: ${response.Email}
 
   `;
   }
